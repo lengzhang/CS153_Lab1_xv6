28d30144993c03adf4b19ba4ec58ba7826cb51c1
Leng <lengzhang@live.com>
2016-10-19 22:38:06 -0700

    finished files transfer

---
+++ xv6-public/zombie.c
@@ -0,0 +1,15 @@
+// Create a zombie process that
+// must be reparented at exit.
+
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int
+main(void)
+{
+  if(fork() > 0)
+    sleep(5);  // Let child exit before parent.
+  exit(0);
+  return(0); // I added this part
+}
